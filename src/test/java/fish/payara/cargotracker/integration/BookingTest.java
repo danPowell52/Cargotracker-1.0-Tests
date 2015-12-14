62f3502c2127f22f3839fa59bed78f42c3e39fe0
danPowell52 <dan.powell@sky.com>
2015-12-14 09:17:08 -0800

    Complete Tests

--- src/test/java/fish/payara/cargotracker/integration/BookingTest.java
+++ src/test/java/fish/payara/cargotracker/integration/BookingTest.java
@@ -1,33 +1,14 @@
 package fish.payara.cargotracker.integration;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlDateInput;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import java.io.IOException;
-import net.java.cargotracker.application.util.DateUtil;
-import net.java.cargotracker.application.util.JsonMoxyConfigurationContextResolver;
-import net.java.cargotracker.domain.model.cargo.*;
-import net.java.cargotracker.domain.model.handling.*;
-import net.java.cargotracker.domain.model.location.Location;
-import net.java.cargotracker.domain.model.location.LocationRepository;
-import net.java.cargotracker.domain.model.location.SampleLocations;
-import net.java.cargotracker.domain.model.location.UnLocode;
-import net.java.cargotracker.domain.model.voyage.*;
-import net.java.cargotracker.domain.service.RoutingService;
-import net.java.cargotracker.domain.shared.*;
-import net.java.cargotracker.infrastructure.persistence.jpa.JpaCargoRepository;
-import net.java.cargotracker.infrastructure.persistence.jpa.JpaHandlingEventRepository;
-import net.java.cargotracker.infrastructure.persistence.jpa.JpaLocationRepository;
-import net.java.cargotracker.infrastructure.persistence.jpa.JpaVoyageRepository;
-import net.java.cargotracker.infrastructure.routing.ExternalRoutingService;
-import net.java.pathfinder.api.GraphTraversalService;
-import net.java.pathfinder.api.TransitEdge;
-import net.java.pathfinder.api.TransitPath;
-import net.java.pathfinder.internal.GraphDao;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -35,7 +16,6 @@ import org.jboss.arquillian.junit.InSequence;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import org.junit.Rule;
 import org.junit.Test;
@@ -46,6 +26,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.Assert;
 import org.junit.Before;
@@ -73,6 +55,8 @@ public class BookingTest {
 
     @ArquillianResource
     private URL deploymentUrl;
+    
+    
 
     @Rule
     public TestName testName = new TestName();
@@ -101,6 +85,9 @@ public class BookingTest {
     public void testBookNewCargo() {
         log.log(Level.INFO, "Starting automated testing to book new cargo.");
         try {
+            //book cargo and set the new cargo ID as the value of newCargoId
+            System.out.println("WUB"+ deploymentUrl);
+            System.out.println("WUB"+ deploymentUrl);
             HtmlPage admin = landingPageResponse.getElementById("adminLandingLink").click();
             Assert.assertThat("Page title was not as expected for the admin dashboard. Expected \"Cargo Dashboard\" but actual was \"" + admin.getTitleText() + "\"." , admin.getTitleText(), is("Cargo Dashboard"));
             HtmlPage makeBooking = admin.getElementById("adminBooking").click();
@@ -109,7 +96,7 @@ public class BookingTest {
             HtmlOption selectDestination = getDestinations.getOptionByText("Tokyo (JNTKO)");
             getDestinations.setSelectedAttribute(selectDestination, true);
             HtmlDateInput dateInput = makeBooking.getElementByName("registrationForm:j_idt20");
-            dateInput.setValueAttribute("2028-06-06");
+            dateInput.setValueAttribute("2016-06-06");
             HtmlPage confirmationPage = makeBooking.getElementByName("registrationForm:j_idt22").click();
             Assert.assertTrue("", confirmationPage.asText().contains("Chicago (USCHI)") );
             Assert.assertTrue("", confirmationPage.asText().contains("Tokyo (JNTKO)") );
@@ -117,6 +104,11 @@ public class BookingTest {
             Object cargoIDPhrase = getID.get(0);
             String [] fragments = cargoIDPhrase.toString().split(" ");
             newCargoId = fragments[3];
+            
+            //route cargo
+            HtmlAnchor anchor = confirmationPage.getAnchorByHref("/cargo-tracker/admin/selectItinerary.xhtml?trackingId="+newCargoId);
+            HtmlPage router = anchor.click();
+            HtmlPage route = router.getElementByName("j_idt16:0:j_idt17:j_idt22").click();
         }
         catch(IOException ie) {
             Assert.fail("An IOException was thrown during the test for class \"" + BookingTest.class.getSimpleName() + "\" at method \"" + testName.getMethodName() + "\" with message: " + ie.getMessage());
@@ -139,7 +131,10 @@ public class BookingTest {
             enterCargoIdPage.getElementById("trackingForm:trackingIdInput").setAttribute("value", newCargoId);
             System.out.println(enterCargoIdPage.getElementById("trackingForm:trackingIdInput").getAttribute("value"));
             HtmlPage trackingPage = enterCargoIdPage.getElementById("trackingForm:submitTrack").click();
-            Assert.assertTrue("Handling history did not show expected first event.", trackingPage.asText().contains("Received in Hong Kong, at 03/01/2014 12:00 AM PST"));
+            Assert.assertTrue("Handling history did not show expected first event.", trackingPage.asText().contains("Cargo "+newCargoId+" is now: Not received"));
+            Assert.assertTrue("Handling history did not show expected second event.", trackingPage.asText().contains("Estimated time of arrival in Tokyo"));
+            Assert.assertTrue("Handling history did not show expected third event.", trackingPage.asText().contains("Next expected activity is to receive cargo in Chicago"));
+
         } catch (IOException ex) {
             Assert.fail("An IOException was thrown during the test for class \"" + BookingTest.class.getSimpleName() + "\" at method \"" + testName.getMethodName() + "\" with message: " + ex.getMessage());
         }
@@ -152,6 +147,22 @@ public class BookingTest {
     @InSequence(3)
     public void testAdminTrackNewCargo() {
         log.log(Level.INFO, "Starting automated test to track new cargo with Id \"" + newCargoId + "\" through admin interface.");
+        log.log(Level.INFO, "Starting automated test to track Id \"" + newCargoId + "\" through admin interface.");
+        try {
+            System.out.println(landingPageResponse.getUrl());
+            HtmlPage adminDashboard = landingPageResponse.getElementById("adminLandingLink").click();
+            Assert.assertThat("Page title was not as expected for the admin dashboard. Expected \"Cargo Dashboard\" but actual was \"" + adminDashboard.getTitleText() + "\"." , adminDashboard.getTitleText(), is("Cargo Dashboard"));
+            HtmlPage enterCargoIdPage = adminDashboard.getElementById("adminTracking").click();
+            enterCargoIdPage.getElementById("trackingForm:trackingIdInput").setAttribute("value", newCargoId);
+            System.out.println(enterCargoIdPage.getElementById("trackingForm:trackingIdInput").getAttribute("value"));
+            HtmlPage trackingPage = enterCargoIdPage.getElementById("trackingForm:submitTrack").click();
+            Assert.assertTrue("Handling history did not show expected first event.", trackingPage.asText().contains("Cargo "+newCargoId+" is now: Not received"));
+            Assert.assertTrue("Handling history did not show expected second event.", trackingPage.asText().contains("Estimated time of arrival in Tokyo"));
+            Assert.assertTrue("Handling history did not show expected third event.", trackingPage.asText().contains("Next expected activity is to receive cargo in Chicago"));
+        } catch (IOException ex) {
+            Assert.fail("An IOException was thrown during the test for class \"" + BookingTest.class.getSimpleName() + "\" at method \"" + testName.getMethodName() + "\" with message: " + ex.getMessage());        }
+    
+    
     }
 
     // TODO Create test to view details of new cargo through the admin interface.
@@ -160,6 +171,21 @@ public class BookingTest {
     @InSequence(4)
     public void testViewDetailsNewCargo() {
         log.log(Level.INFO, "Starting automated test to view details for new cargo with Id \"" + newCargoId + "\" through admin interface.");
+        try {
+            System.out.println(landingPageResponse.getUrl());
+            //Stores the adminDashboard as a HtmlPage object.
+            HtmlPage adminDashboard = landingPageResponse.getElementById("adminLandingLink").click();
+            Assert.assertThat("Page title was not as expected for the admin dashboard. Expected \"Cargo Dashboard\" but actual was \"" + adminDashboard.getTitleText() + "\"." , adminDashboard.getTitleText(), is("Cargo Dashboard"));
+            //Stores the details page as a HtmlPage object.           
+            HtmlPage detailsPage = adminDashboard.getAnchorByText(newCargoId).click();
+            System.out.println(detailsPage.asText());
+            log.log(Level.INFO, detailsPage.asText());
+
+            Assert.assertTrue("Origin was not as expected", detailsPage.asText().contains("Origin	Chicago (USCHI)"));
+            Assert.assertTrue("Destination was not as expected", detailsPage.asText().contains("Destination	Tokyo (JNTKO)"));
+        } catch (IOException ex) {
+            Assert.fail("An IOException was thrown during the test for class \"" + BookingTest.class.getSimpleName() + "\" at method \"" + testName.getMethodName() + "\" with message: " + ex.getMessage());
+        }
     }
 
     // TODO Create test to change the destination of the new cargo through the admin interface.
@@ -168,5 +194,24 @@ public class BookingTest {
     @InSequence(5)
     public void testChangeEndNewCargo() {
         log.log(Level.INFO, "Starting automated test to change destination for new cargo with Id \"" + newCargoId + "\" through the admin interface.");
+        
+        
+        try {
+            System.out.println(landingPageResponse.getUrl());
+            HtmlPage adminDashboard = landingPageResponse.getElementById("adminLandingLink").click();
+            Assert.assertThat("Page title was not as expected for the admin dashboard. Expected \"Cargo Dashboard\" but actual was \"" + adminDashboard.getTitleText() + "\"." , adminDashboard.getTitleText(), is("Cargo Dashboard"));
+            HtmlPage detailsPage = adminDashboard.getAnchorByText(newCargoId).click();
+            Assert.assertThat("Page title was not as expected for the admin dashboard. Expected \"Cargo Administration\" but actual was \"" + detailsPage.getTitleText() + "\".", detailsPage.getTitleText(), is("Cargo Administration"));
+            HtmlPage destinationPage = detailsPage.getAnchorByText("Change destination").click();
+        
+            HtmlSelect getDestinations = destinationPage.getElementByName("j_idt14:j_idt16");
+            HtmlOption selectDestination = getDestinations.getOptionByText("Stockholm (SESTO)");
+            getDestinations.setSelectedAttribute(selectDestination, true);
+            HtmlPage confirmationPage = destinationPage.getElementByName("j_idt14:j_idt19").click();
+            Assert.assertTrue("Destination was not as expected", confirmationPage.asText().contains("Destination	Stockholm (SESTO)"));
+        } catch (IOException ex) {
+            Assert.fail("An IOException was thrown during the test for class \"" + BookingTest.class.getSimpleName() + "\" at method \"" + testName.getMethodName() + "\" with message: " + ex.getMessage());
+        }
+        
     }
 }
